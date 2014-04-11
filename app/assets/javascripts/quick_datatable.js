a300272bae11ff45e0d105b2335e206eb9d9b3d2
Nahuel Sciaratta <nahuelsciaratta@gmail.com>
2014-04-11 11:24:43 -0300

    Remove jquery.ui dependency, and rewrite plugin

--- app/assets/javascripts/quick_datatable.js
+++ app/assets/javascripts/quick_datatable.js
@@ -1,7 +1,7 @@
-// 
-// Requirements:
-//   jquery.Datatables
-//   fnMultiFilter
+//= require dataTables/jquery.dataTables
+//= require multi_filter
+
+
 // Usage:
 // 
 //  $('table.data_table').quick_datatable()
@@ -16,29 +16,19 @@
 //    }
 //  });
 
-( function( $ ) {
-    $.widget( 'fn.quick_datatable', {
-      options:{
-        data_table_options:{}
-      },
-      vars:{
-        columns:[],
-        //current datatable
-        data_table: null, 
-        default_datatable_options:{
-          bProcessing: true,
-          bServerSide: true,
-          sStripeOdd: '',
-          sStripeEven: '',
-          bLengthChange: true,
-          sDom: 'rtip'
-        }
-      },
-      _create: function (){
-        this.vars = {
+
+(function ( $ ) {
+
+  var private_vars = null;
+  $.fn.QuickDatatable = function(options) {
+      
+     var settings = $.extend({
+          // These are the defaults.
           columns:[],
-          //current datatable
-          data_table: null, 
+          search_button: null,
+          results_per_page: null,
+          fields_for_search: null,
+          data_table: null,
           default_datatable_options:{
             bProcessing: true,
             bServerSide: true,
@@ -47,69 +37,73 @@
             bLengthChange: true,
             sDom: 'rtip'
           }
+      }, options );
+
+    return this.each(function() {
+         var $this = $( this );
+        _build_columns_attrs($this);
+        _build_datatable($this);
+
+        _bind_search($this);
+        _bind_results_per_page();
+        _bind_enter_key();
+    });
+
+    function _build_columns_attrs($obj){
+      settings.columns = []
+      $.each($obj.find("th"), function(i, col){
+        var column_options = {};
+        $.each($(col).data(), function (key, value){
+          column_options[key] = value;
+        });
+        settings.columns.push(column_options);
+      });
+    }
+    function _build_datatable($obj) {
+      var data_table_options = {
+        sAjaxSource: $obj.data('source'),
+        aoColumns: settings.columns,
+        iDisplayLength: parseInt($(settings.results_per_page).val())
+      }
+
+      var data_table_options_merged = $.extend(data_table_options,$obj.find("thead tr").data(), settings.default_datatable_options, settings.data_table_options);
+      settings.data_table = $obj.dataTable(data_table_options_merged);
+    }
+
+    function _bind_search($obj){
+      
+      $(settings.button_search).on('click', function (){
+        var filters = {};
+        $.each($(settings.fields_for_search), function (i, elem){
+          name = $(elem).attr('name');
+          value = $(elem).val();
+          filters[name] = value;
+        });
+        var oSettings = settings.data_table.fnSettings();
+        oSettings._iDisplayLength = parseInt($(settings.results_per_page).val());
+        settings.data_table.fnMultiFilter(filters);
+      });
+    }
+
+    function _bind_results_per_page() {
+      $(settings.results_per_page).on('keypress', function (e){
+        if(e.which == 13) {
+          var oSettings = settings.data_table.fnSettings();
+          oSettings._iDisplayLength = parseInt($(this).val());
+          $(settings.button_search).trigger("click");
         }
+      });
+    }
 
-        this._build_columns_attrs();
-        this._build_datatable();
-
-        this._bind_search();
-        this._bind_results_per_page();
-        this._bind_enter_key();
-      },
-      //get data attr from <th> tag
-      _build_columns_attrs: function (){
-        var self = this;
-        self.vars.columns = []
-        $.each($(this.element).find("th"), function(i, col){
-          var column_options = {};
-          $.each($(col).data(), function (key, value){
-            column_options[key] = value;
-          });
-          self.vars.columns.push(column_options);
-        });
-      },
-      _build_datatable: function () {
-        var data_table_options = {
-          sAjaxSource: $(this.element).data('source'),
-          aoColumns: this.vars.columns,
-          iDisplayLength: parseInt($(this.options.results_per_page).val())
+    function _bind_enter_key() {
+      $(settings.fields_for_search).keypress(function (e){
+        if(e.which == 13) {
+          $(settings.button_search).trigger("click");
         }
+      });
+    }
+   
+  };
 
-        var data_table_options_merged = $.extend(data_table_options,$(this.element).find("thead tr").data(), this.vars.default_datatable_options, this.options.data_table_options);
-        this.vars.data_table = $(this.element).dataTable(data_table_options_merged);
-      },
-      _bind_search: function() {
-        var self = this;
-        $(this.options.button_search).on('click', function (){
-          var filters = {};
-          $.each($(self.options.fields_for_search), function (i, elem){
-            name = $(elem).attr('name');
-            value = $(elem).val();
-            filters[name] = value;
-          });
-          var oSettings = self.vars.data_table.fnSettings();
-          oSettings._iDisplayLength = parseInt($(self.options.results_per_page).val());
-          self.vars.data_table.fnMultiFilter(filters);
-        });
-      },
-      _bind_results_per_page: function () {
-        var self = this;
-        $(this.options.results_per_page).on('keypress', function (e){
-          if(e.which == 13) {
-            var oSettings = self.vars.data_table.fnSettings();
-            oSettings._iDisplayLength = parseInt($(this).val());
-            $(self.options.button_search).trigger("click");
-          }
-        });
-      },
-      _bind_enter_key: function () {
-        var self = this;
-        $(this.options.fields_for_search).keypress(function (e){
-          if(e.which == 13) {
-            $(self.options.button_search).trigger("click");
-          }
-        });
-      },
+}(jQuery));
 
-    })
-})( jQuery );
