184c3b749de441be8d05ca697fbe7b0f407dacab
Nahuel Sciaratta <nahuelsciaratta@gmail.com>
2014-04-11 11:25:14 -0300

    moved to vendor

---
+++ vendor/assets/javascripts/multi_filter.js
@@ -0,0 +1,18 @@
+$.fn.dataTableExt.oApi.fnMultiFilter = function( oSettings, oData ) {
+    for ( var key in oData )
+    {
+        if ( oData.hasOwnProperty(key) )
+        {
+            for ( var i=0, iLen=oSettings.aoColumns.length ; i<iLen ; i++ )
+            {
+                if( oSettings.aoColumns[i].sName == key )
+                {
+                    /* Add single column filter */
+                    oSettings.aoPreSearchCols[ i ].sSearch = oData[key];
+                    break;
+                }
+            }
+        }
+    }
+    this.oApi._fnDraw( oSettings );
+};
